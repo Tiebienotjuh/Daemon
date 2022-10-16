d6c41703496c1776dc0c8b621b9b6b3e35ca79bb
Tiebienotjuh <tiebe.seynhaeve@sgsintpaulus.eu>
2022-10-16 17:40:07 +0200

    created admin route

--- api/admin.js
+++ api/admin.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const config = require("../config.json");
+const fs = require("fs");
 const { auth } = require("../functions/auth");
 
 const router = express.Router()
+
+router.post("/create/:uuid", (req, res) => {
+    auth(req, res, () => {
+        let path = config.system.path + '/' + req.query.uuid
+        if(fs.existsSync(path)) return res.status(500).json({error: "Server already exists"}); {
+            fs.mkdirSync(path);
+            res.status(200).json({data: "Server created"});
+        }
+    });
+  });
+
+router.post("/delete/:uuid", (req, res) => {
+    auth(req, res, () => {
+        let path = config.system.path + '/' + req.query.uuid
+        if(!fs.existsSync(path)) return res.status(500).json({error: "Server doesn't exists"}); {
+            fs.unlink(path);
+            res.status(200).json({data: "Server deleted"});
+        }
+    });
+  });
 
 module.exports = router
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const fs = require("fs");
 
-const config = require("./config.json")
+const config = require("./config.json");
+const { auth } = require("./functions/auth");
 var app = express();
 
 app.use(bodyParser.json());
@@ -11,8 +12,16 @@ app.use(cors());
 
 
 fs.readdirSync('./api/').filter((file) => file.endsWith('.js')).forEach((apiroute) => {
-    app.use('/' + apiroute.split(".")[0], require(`./api/${apiroute}`));
+    app.use('/api/' + apiroute.split(".")[0], require(`./api/${apiroute}`));
 })
+
+app.get('/', auth, (req, res) => {
+    res.status(200).json({status: "ok"});
+});
+
+app.get('*', auth, (req, res) => {
+    res.status(404).json({error: "Route doesn't exists"});
+});
 
 if(!config.api.token) {
     console.log("There was no API Token found in the config.json file. Please add one and restart the daemon.");
