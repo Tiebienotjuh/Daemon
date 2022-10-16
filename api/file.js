1d5d06142c8b8e5488f693ff980b0332f711b121
Tiebienotjuh <tiebe.seynhaeve@sgsintpaulus.eu>
2022-10-16 15:09:20 +0200

    Update file.js

--- api/file.js
+++ api/file.js
@@ -4,16 +4,28 @@ const { auth } = require("../functions/auth");
 
 const router = express.Router()
 
-router.post("/save", (req, res) => {
-    auth(req, res, () => {
-      let path = config.system.path + '/' + req.body.uuid + '/' + req.body.path + '/' + req.body.file;
+router.post("/save/:uuid", (req, res) => {
+  auth(req, res, () => {
+      let path = config.system.path + '/' + req.query.uuid + '/' + req.body.path + '/' + req.body.file;
       try {
         fs.writeFileSync(path, req.body.data);
-        res.status(200).json({success: true});
+        res.status(200);
       } catch (error) {
         res.status(500).json({error: error});
       }
     });
+});
+
+router.post("/open/:uuid", (req, res) => {
+  auth(req, res, () => {
+    let path = config.system.path + '/' + req.query.uuid + '/' + req.body.path + '/' + req.body.file;
+    try {
+      if (!fs.existsSync(path)) return res.status('404').json({error: "File not found"});
+      res.status(200).json({data: fs.readFileSync(path, "utf8")});
+    } catch (error) {
+      res.status(500).json({error: error});
+    }
+  });
 });
 
 module.exports = router
